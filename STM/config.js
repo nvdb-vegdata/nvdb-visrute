e1b4e47ac7c87a6124c9b7c87f6d1efb24805488
hansog <hansog@outlook.com>
2020-03-26 13:45:51 +0100

    Fixed server urls for STM

--- STM/config.js
+++ STM/config.js
@@ -1,8 +1,6 @@
 let serverURLs = [
-    {url: "https://nvdbapiles-v3.utv.atlas.vegvesen.no", label: "https://nvdbapiles-v3.utv.atlas.vegvesen.no (STM)"},
-    {url: "https://www.utv.vegvesen.no/nvdb/api/v3", label: "https://www.utv.vegvesen.no/nvdb/api/v3 (STM)"},
-    {url: "https://nvdbw01.kantega.no/nvdb/api/v3", label: "https://nvdbw01.kantega.no/nvdb/api/v3 (K2 - Kantega)"},
-    {url: "http://localhost:12002", label: "http://localhost:12002 (localhost for developers)"}
+    {url: "https://nvdbapiles-v3-stm.utv.atlas.vegvesen.no", label: "https://nvdbapiles-v3-stm.utv.atlas.vegvesen.no (STM)"},
+    {url: "https://pm1.utv.vegvesen.no/nvdb/api/v3", label: "https://pm1.utv.vegvesen.no/nvdb/api/v3 (STM)"},
 ];
 
 serverURLs.forEach(v => {
