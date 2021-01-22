37adfc9920943acb98613ee36ae4deb4bbc52645
Kristoffer Mjølhus <kristoffer.mjolhus@kantega.no>
2021-01-22 14:08:53 +0100

    Adjusting map loading STM, ATM and PROD, https://blog.mapbox.com/deprecating-studio-classic-styles-d8892ac38cb4

--- ATM/scripts.js
+++ ATM/scripts.js
@@ -1,10 +1,12 @@
-const BACKGROUND_LAYER = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+const BACKGROUND_LAYER = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © ' +
+        '<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
+        '<strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     maxZoom: 22,
     maxNativeZoom: 19,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.streets',
+    tileSize: 512,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 });
 
