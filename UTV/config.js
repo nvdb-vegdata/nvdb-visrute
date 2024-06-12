05b9d69a9284a8ceec1f45b049c8e0b2c00702dd
Geir Sagberg <geir.sagberg@gmail.com>
2024-06-12 09:13:01 +0200

    Add URL for V4

--- UTV/config.js
+++ UTV/config.js
@@ -1,11 +1,18 @@
 let serverURLs = [
-    {url: "https://nvdbapiles-v3.utv.atlas.vegvesen.no", label: "https://nvdbapiles-v3.utv.atlas.vegvesen.no (UTV)"},
-    {url: "http://localhost:12002", label: "http://localhost:12002 (localhost for developers)"}
-];
+  {
+    url: 'https://nvdbapiles-v3.utv.atlas.vegvesen.no',
+    label: 'https://nvdbapiles-v3.utv.atlas.vegvesen.no (UTV)',
+  },
+  {
+    url: 'https://nvdbapiles.utv.atlas.vegvesen.no',
+    label: 'https://nvdbapiles.utv.atlas.vegvesen.no (V4 UTV)',
+  },
+  {
+    url: 'http://localhost:12002',
+    label: 'http://localhost:12002 (localhost for developers)',
+  },
+]
 
-serverURLs.forEach(v => {
-    $("#server")
-        .append($("<option>", { value : v.url})
-        .text(v.label));
-});
-
+serverURLs.forEach((v) => {
+  $('#server').append($('<option>', { value: v.url }).text(v.label))
+})
